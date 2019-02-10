bf68185beadbc0f26d988f7febad5186f744dc11
Trần khánh thiện <47145219+thientt03@users.noreply.github.com>
2019-02-10 23:54:24 +0700

    Add files via upload

---
+++ sheep5.py
@@ -0,0 +1,15 @@
+print("Hello, My name is thien and these are my ship size")
+sheeplist = [5, 7, 300, 90, 24, 50, 75] 
+x = input("Nhập số tháng : ")
+loop = True
+while loop:
+    sheeplist = [50 + x for x in sheeplist]
+    sheeplist.sort()
+    
+    if sheeplist[0] == 350:
+        sheeplist[0] = 8
+        sheeplist.sort()
+        loop = False
+        print(sheeplist)
+
+    
